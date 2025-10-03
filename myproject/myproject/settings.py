36f29527d721381a74490b767ca8f2f8e3818754
solen <kassahunsolen@gmail.com>
2025-10-03 21:35:10 +0300

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -93,7 +93,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
+    }, 
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
