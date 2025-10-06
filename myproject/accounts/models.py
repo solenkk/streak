09351e5852bb26da8e9a3394c98f37d550ef0329
solen <kassahunsolen@gmail.com>
2025-10-06 22:09:53 +0300

    Update models.py

--- myproject/accounts/models.py
+++ myproject/accounts/models.py
@@ -2,3 +2,4 @@ from django.db import models
 # abet yesu sira 
 # Create your models here.
 #model is something like table 
+one key take away of today is that why we use model.py i really got some ideas so i losved how it work how it align with serilezer.py 
